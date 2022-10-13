0cfd66d41abb8cc04f4ac0215497d30a7d7dd256
Vladislav Oganezov <oganezovvd@gmail.com>
2022-10-13 16:41:11 +0300

    Coupons are created. Also export to csv files was added to adminsite

--- geekochad/settings.py
+++ geekochad/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'users',
     'cart',
     'orders',
+    'payment',
+    'coupons',
     
 ]
 
--- orders/tasks.py
+++ orders/tasks.py
@@ -9,9 +9,10 @@ def order_created(order_id):
     subject = f'Заказ № {order.id}'
     message = f'Дорогой(ая) {order.first_name}, \n\n' \
               f'Ваш заказ успешно размещен. \n' \
-              f'Номер заказа - {order.id}.'
+              f'Номер заказа - {order.id}. \n\n' \
+              f'C уважением, команда Geekochad.'
     mail_sent = send_mail(subject,
                           message,
-                          'admin@geekochad.com',
+                          'geekochad@gmail.com',
                           [order.email])
     return mail_sent
